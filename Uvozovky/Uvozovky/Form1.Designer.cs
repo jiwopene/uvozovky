d601dc9ccb965d433d3471e849636c911fc06b4c
jiwopene <jiwopene@gmail.com>
2016-11-07 16:32:21 +0100

    Zdroják

---
+++ Uvozovky/Uvozovky/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Uvozovky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dolni = new System.Windows.Forms.NotifyIcon(this.components);
+            this.horni = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // dolni
+            // 
+            this.dolni.Icon = ((System.Drawing.Icon)(resources.GetObject("dolni.Icon")));
+            this.dolni.Text = "Začátek";
+            this.dolni.Visible = true;
+            this.dolni.Click += new System.EventHandler(this.dolni_Click);
+            this.dolni.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dolni_MouseDoubleClick);
+            // 
+            // horni
+            // 
+            this.horni.Icon = ((System.Drawing.Icon)(resources.GetObject("horni.Icon")));
+            this.horni.Text = "Konec";
+            this.horni.Visible = true;
+            this.horni.Click += new System.EventHandler(this.horni_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(0, 0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon dolni;
+        private System.Windows.Forms.NotifyIcon horni;
+    }
+}
+
---
+++ Uvozovky/Uvozovky/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uvozovky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+            InitializeComponent();
+        }
+
+        private void dolni_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void dolni_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("%{TAB}");
+            Thread.Sleep(100);
+            SendKeys.Send("„");
+        }
+
+        private void horni_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("%{TAB}");
+            Thread.Sleep(100);
+            SendKeys.Send("“");
+        }
+    }
+}
